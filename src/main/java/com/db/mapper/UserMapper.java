f7aa9fc251e6383856dc9052c0653b12c8755c41
GreenteaPIE <secret48156@gmail.com>
2023-05-10 16:34:49 +0900

    commit test

---
+++ src/main/java/com/db/mapper/UserMapper.java
@@ -0,0 +1,5 @@
+package com.db.mapper;
+
+public interface UserMapper {
+
+}
--- src/main/java/com/db/model/UserVO.java
+++ src/main/java/com/db/model/UserVO.java
@@ -5,9 +5,9 @@ import java.sql.Timestamp;
 import lombok.Data;
 
 @Data
-public class ShopUserVO {
+public class UserVO {
 
-	private String userid, pass, name, email, address1, address2, phone, zipcode;
+	private String userid, pass, name, email, address1, address2, address3, phone;
 	private int gender, point, grade;
 	private Timestamp enter;
 
